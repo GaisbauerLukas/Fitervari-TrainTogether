5d74442951ef9d2ade80e8d607cd30c2f54978ce
florentin <florentingewessler@gmail.com>
2020-05-18 19:53:13 +0200

    Add annotations to SetHistory.kt

--- Code/fitervari_backend/src/main/java/at/htl_leonding/model/SetHistory.kt
+++ Code/fitervari_backend/src/main/java/at/htl_leonding/model/SetHistory.kt
@@ -1,14 +1,18 @@
 package at.htl_leonding.model
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity
-import javax.persistence.ManyToOne
+import javax.persistence.*
 
+@Entity
+@Table(name = "set_history")
 data class SetHistory (
         @ManyToOne
+        @JoinColumn(name = "exersice_history_id")
         var exerciseHistory: ExerciseHistory,
         var time: Double,
         var distance: Double,
         var weight: Double,
         var repetitions: Int,
+        @Column(name = "set_number")
         var setNumber: Int
 ) : PanacheEntity()
