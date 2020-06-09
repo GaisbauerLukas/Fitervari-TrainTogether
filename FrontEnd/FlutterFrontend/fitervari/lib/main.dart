c0b6d65e18786ae69dbb96ec3410fd4a5571ed31
florentingewessler <florentingewessler@gmail.com>
2020-06-09 07:16:37 +0200

    Chagne version of provider package because of incompatability. TODO work on this later

--- FrontEnd/FlutterFrontend/fitervari/lib/main.dart
+++ FrontEnd/FlutterFrontend/fitervari/lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:fitervari/views/tabs_screen/tabs_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './views/tabs_screen/tabs_screen.dart';
+import './logic/providers/news_letters_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,28 +13,32 @@ class MyApp extends StatelessWidget {
 
   static const mainColorCode = 0xFF170D3D;
 
-  Map<int, Color> mainColor =
-  {
-    50:Color.fromRGBO(23,13,61, .1),
-    100:Color.fromRGBO(23,13,61, .2),
-    200:Color.fromRGBO(23,13,61, .3),
-    300:Color.fromRGBO(23,13,61, .4),
-    400:Color.fromRGBO(23,13,61, .5),
-    500:Color.fromRGBO(23,13,61, .6),
-    600:Color.fromRGBO(23,13,61, .7),
-    700:Color.fromRGBO(23,13,61, .8),
-    800:Color.fromRGBO(23,13,61, .9),
-    900:Color.fromRGBO(23,13,61, 1),
+  Map<int, Color> mainColor = {
+    50: Color.fromRGBO(23, 13, 61, .1),
+    100: Color.fromRGBO(23, 13, 61, .2),
+    200: Color.fromRGBO(23, 13, 61, .3),
+    300: Color.fromRGBO(23, 13, 61, .4),
+    400: Color.fromRGBO(23, 13, 61, .5),
+    500: Color.fromRGBO(23, 13, 61, .6),
+    600: Color.fromRGBO(23, 13, 61, .7),
+    700: Color.fromRGBO(23, 13, 61, .8),
+    800: Color.fromRGBO(23, 13, 61, .9),
+    900: Color.fromRGBO(23, 13, 61, 1),
   };
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Fitervari',
-      theme: ThemeData(
-        primarySwatch: MaterialColor(mainColorCode, mainColor),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return ChangeNotifierProvider(
+      builder: (ctx) => NewsLettersProvider(),
+      child: MaterialApp(
+        title: 'Fitervari',
+        theme: ThemeData(
+          primarySwatch: MaterialColor(mainColorCode, mainColor),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: TabsScreen(),
+        routes: {},
       ),
-      home: TabsScreen(),
     );
   }
 }
