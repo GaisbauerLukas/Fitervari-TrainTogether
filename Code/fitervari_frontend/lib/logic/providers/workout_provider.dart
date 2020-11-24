0df032b09eaf4169ba51f2f25ef0dd414b5dca15
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2020-11-24 13:16:25 +0100

    Added Workout_Provider

--- Code/fitervari_frontend/lib/logic/providers/workout_provider.dart
+++ Code/fitervari_frontend/lib/logic/providers/workout_provider.dart
@@ -2,14 +2,20 @@ import 'package:fitervari/contracts/transfer/workout.dart';
 import 'package:flutter/material.dart';
 
 class WorkoutProvider extends ChangeNotifier {
+  static const url = 'http://10.0.2.2:8080/api/workout';
+
   Workout _nextWorkout;
-  List<Workout> _loadedWorkouts;
+  List<Workout> _items = [];
 
   Workout get nextWorkout =>_nextWorkout;
-  List<Workout> get loadedWorkouts => _loadedWorkouts;
+  List<Workout> get getWorkouts => _items;
 
 
   void setNextWorkout(Workout workout) {
     _nextWorkout = workout;
+  }
+
+  List<Workout> get items {
+    return [..._items];
   }
 }
