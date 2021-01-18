ff5bf458c927053dae9eaf9a7bfef7b07dc6c141
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2021-01-18 22:02:06 +0100

    Added Delete Button

--- Code/fitervari_frontend/lib/views/training_info/sub_widgets/my_workouts.dart
+++ Code/fitervari_frontend/lib/views/training_info/sub_widgets/my_workouts.dart
@@ -1,3 +1,5 @@
+import 'package:fitervari/views/filler_page/filler_page.dart';
+import 'package:fitervari/views/workout_edit_view/workout_edit_view.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:fitervari/contracts/transfer/workout.dart';
@@ -11,7 +13,6 @@ class MyWorkouts extends StatelessWidget {
         builder: (context, workoutProvider, child) {
       List<Workout> workouts = workoutProvider.loadedWorkouts;
 
-
       return Container(
           margin: EdgeInsets.only(top: 10),
           height: (MediaQuery.of(context).size.height) * 0.40,
@@ -19,18 +20,32 @@ class MyWorkouts extends StatelessWidget {
             scrollDirection: Axis.vertical,
             itemBuilder: (ctx, index) {
               return Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      ListTile(
-                        // leading: Icon(Icons.directions_run),
-                        title: Text(workouts[index].name),
-                        //subtitle: Text(workouts[index].creator.name),
-                      ),
-
-                    ],
+                child: InkWell(
+                  splashColor: Colors.blue.withAlpha(30),
+                  onTap: () {
+                    print('Card tapped.');
+                    Navigator.of(context).pushNamed(WorkoutEditView.routeName);
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        ListTile(
+                          // leading: Icon(Icons.directions_run),
+                          title: Text(workouts[index].name),
+                          //subtitle: Text(workouts[index].creator.name),
+                          trailing: IconButton(
+                            icon: Icon(Icons.delete, color: Colors.red),
+                            onPressed: () {
+                              print('Delete tapped.');
+                              Navigator.of(context)
+                                  .pushNamed(FillerPage.routeName);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               );
