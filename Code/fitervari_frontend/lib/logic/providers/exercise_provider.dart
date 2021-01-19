f229bcdacf6b9d58dd8efde4faf28e9ca39b484d
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2021-01-19 09:19:49 +0100

    Added Exercise provider

---
+++ Code/fitervari_frontend/lib/logic/providers/exercise_provider.dart
@@ -0,0 +1,22 @@
+import 'package:fitervari/contracts/transfer/exercise.dart';
+import 'package:fitervari/logic/network/exercise_endpoint.dart';
+import 'package:flutter/material.dart';
+
+class ExerciseProvider extends ChangeNotifier{
+  List<Exercise> _exercises;
+
+  List<Exercise> get exercises => _exercises;
+
+  ExerciseEndpoint _endpoint;
+
+  ExerciseProvider(){
+    _endpoint = ExerciseEndpoint();
+    _exercises = [];
+  }
+
+  loadExercises() async{
+    var tmp = await _endpoint.getAll();
+    _exercises.addAll(tmp);
+  }
+
+}
