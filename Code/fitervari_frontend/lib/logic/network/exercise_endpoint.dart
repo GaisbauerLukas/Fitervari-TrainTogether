a571a04ddabdf11e82cbad4309152fef3dfb02f3
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2021-01-19 10:32:27 +0100

    Added baseUrl to ExerciseEndpoint

--- Code/fitervari_frontend/lib/logic/network/exercise_endpoint.dart
+++ Code/fitervari_frontend/lib/logic/network/exercise_endpoint.dart
@@ -1,7 +1,11 @@
+import 'package:fitervari/contracts/application_properties.dart';
 import 'package:fitervari/contracts/transfer/exercise.dart';
 import 'package:fitervari/logic/network/generic_endpoint.dart';
 
 class ExerciseEndpoint extends GenericEndpoint<Exercise>{
+  ExerciseEndpoint(){
+    this.baseUrl = ApplicationProperties.url + 'exercises/';
+  }
   @override
   Exercise convertJsonToObject(dataItem) {
     return Exercise.fromJson(dataItem);
