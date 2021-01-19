787eec939a5113431a0dce358b5e080af5901551
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2021-01-19 10:10:36 +0100

    Exercise List to Statefull Widget

--- Code/fitervari_frontend/lib/views/workout_edit_view/sub_widgets/exercise_list.dart
+++ Code/fitervari_frontend/lib/views/workout_edit_view/sub_widgets/exercise_list.dart
@@ -1,16 +1,43 @@
+import 'package:fitervari/contracts/transfer/exercise.dart';
 import 'package:fitervari/logic/providers/exercise_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class ExerciseList extends StatelessWidget {
+import 'exercise_tile.dart';
+
+class ExerciseList extends StatefulWidget {
   static const routeName = "/Exercise_List";
 
+  @override
+  State<StatefulWidget> createState() {
+    return ExerciseRoutineState();
+  }
+}
+class ExerciseRoutineState extends State<ExerciseList>{
+  List<Exercise> exercises;
+  @override
+  void initState(){
+    exercises = [];
+    Provider.of<ExerciseProvider>(context, listen: false).loadExercises();
+  }
+  @override
+  void setState(fn) {
+    super.setState(fn);
+  }
   @override
   Widget build(BuildContext context) {
     return Consumer<ExerciseProvider>(
       builder: (context, provider, child) {
+        exercises = provider.exercises;
         return Scaffold(
           appBar: AppBar(),
+          body: ListView.builder(
+            scrollDirection: Axis.vertical,
+            itemBuilder: (ctx, index) {
+              return ExerciseTile(exercises[index]);
+            },
+            itemCount: exercises.length,
+          ),
         );
       },
     );
