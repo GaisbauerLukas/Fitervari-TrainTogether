4df8025b3c8d05ad56c0f39fd922e32c59df43ba
Gaisbauer Lukas <gaisbauer.lukas@gmx.at>
2021-01-24 20:36:28 +0100

    Added Create Exercise View

---
+++ Code/fitervari_frontend/lib/views/exercise_view/create_exercise.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CreateExercise extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: ListView(children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+                decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Name',
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Übungs Typ',
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Standart Anzahl von Sätzen',
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
