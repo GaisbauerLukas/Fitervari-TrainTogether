c2f3bb6088c3a127647e43819e980638335132a0
Christoph Knoll <c.knoll@htblaleonding.ac.at>
2021-01-24 18:49:03 +0100

    Refactor (add) create_workout_button.dart

---
+++ Code/fitervari_frontend/lib/views/training_info/sub_widgets/create_workout_button.dart
@@ -0,0 +1,37 @@
+import 'package:fitervari/logic/helper/session_info.dart';
+import 'package:fitervari/views/create_workout_page/create_workout_page.dart';
+import 'package:fitervari/views/landing_page/sub_widgets/choose_workout_bottom_sheet.dart';
+import 'package:flutter/material.dart';
+
+class CreateWorkoutButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () =>
+          Navigator.of(context).pushNamed(CreateWorkoutPage.routeName),
+      child: Card(
+        margin: EdgeInsets.all(10),
+        color: Theme.of(context).primaryColor,
+        elevation: 4,
+        child: Container(
+          margin: EdgeInsets.all(20),
+          height: (MediaQuery.of(context).size.height -
+              SessionInfo().actionBarHeight) *
+              0.04,
+          width: double.infinity,
+          child: Center(
+            child: Text(
+              'Neues Workout erstellen',
+              style: TextStyle(
+                  fontSize: (MediaQuery.of(context).size.height -
+                      SessionInfo().actionBarHeight) *
+                      0.04,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w100),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
